7fc4a74f44768a9d8a062e21236a4ffa51ecf8f2
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-07-04 15:10:28 +0100

    Added schema for users

--- models/users.js
+++ models/users.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose')
-const schema = mongoose.Schema({
+const moogoose = require (moogoose)
+const schema = moogoose.schema({
     fullname: {
         type:String,
-        required:true
+        required: true
     },
-    username: {
+    lastname: {
         type:String,
-        required:true
+        required: true
     },
     email: {
         type:String,
-        required:true
+        required: true
     },
-    password: {
-        type:String,
-        required:true
-    }
-   })
+    password: String,
+    required: true
+})
 
-module.exports = mongoose.model('user',schema)
+module.exports = moogoose.models('users', schema)
